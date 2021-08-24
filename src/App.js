b1ca4e2ad70f79963f8ae37ce4c4faae2df5e185
Ali Eslam <alizaeda92@gmail.com>
2021-08-24 06:13:39 +0200

    myReads Entire App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './style/App.css';
+import SearchInput from './Components/SearchInput';
+import MyReads from './Components/MyReads';
+import { Route } from 'react-router-dom';
+import * as BooksAPI from './API/BooksAPI';
+import getAll from './Components/data';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  BookShelf = ['Currently Reading', 'Want to read', 'Read'];
+  state = {
+    Books: [],
+    err: false,
+    books: getAll,
+  };
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then(book => {
+        this.setState({ Books: book });
+        // console.log('====================================');
+        // console.log(book);
+        // console.log('====================================');
+      })
+      .catch(err => this.setState({ err: true }));
+  }
+  render() {
+    return (
+      <div className="app">
+        <Route path="/search" component={SearchInput} />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <MyReads BookShelf={this.BookShelf} books={this.state.books} />
+          )}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// {/* <SearchBtn /> */}
